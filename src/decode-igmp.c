0b1cfd01233e6125c217c9e2c3abb953df18e121
shihb <shihb0416121210@163.com>
2025-09-28 16:07:00 +0800

    fix: format

--- src/decode-igmp.c
+++ src/decode-igmp.c
@@ -13,11 +13,9 @@
 #include "decode-igmp.h"
 #include "flow.h"
 
-
 #include "util-validate.h"
 #include "util-unittest.h"
 #include "util-debug.h"
-
 
 static uint16_t IgmpCalcChecksum(const uint8_t *data, size_t len)
 {
@@ -25,49 +23,43 @@ static uint16_t IgmpCalcChecksum(const uint8_t *data, size_t len)
     size_t i = 0;
 
     // 以16位为单位累加
-    for (i = 0; i < len; i += 2)
-    {
+    for (i = 0; i < len; i += 2) {
         uint16_t word = ((data[i] << 8) | (i + 1 < len ? data[i + 1] : 0));
         sum += word;
     }
 
     // 处理进位
-    while (sum >> 16)
-    {
+    while (sum >> 16) {
         sum = (sum & 0xffff) + (sum >> 16);
     }
 
     return ~sum;
 }
 
-
-static int IgmpParseV3Query(Packet *p,const uint8_t *buffer, size_t len)
+static int IgmpParseV3Query(Packet *p, const uint8_t *buffer, size_t len)
 {
     int iRet = -1;
-    if (len < sizeof(IGMPV3QueryHdr) - sizeof(uint32_t *))
-    {
+    if (len < sizeof(IGMPV3QueryHdr) - sizeof(uint32_t *)) {
         ENGINE_SET_INVALID_EVENT(p, IGMP_MSG_LEN_TOO_SMALL);
         return iRet;
     }
 
     return TM_ECODE_OK;
 }
-static int ParseIGMPV3Report(Packet *p,const uint8_t *buffer, size_t len)
+static int ParseIGMPV3Report(Packet *p, const uint8_t *buffer, size_t len)
 {
     int iRet = -1;
-    if (len < sizeof(IGMPV3ReportHdr) - sizeof(IGMPV3GroupRecord *))
-    {
+    if (len < sizeof(IGMPV3ReportHdr) - sizeof(IGMPV3GroupRecord *)) {
         ENGINE_SET_INVALID_EVENT(p, IGMP_MSG_LEN_TOO_SMALL);
         return iRet;
     }
     return TM_ECODE_OK;
 }
 
-static int IgmpParseV1V2(Packet *p,const uint8_t *buffer, size_t len)
+static int IgmpParseV1V2(Packet *p, const uint8_t *buffer, size_t len)
 {
     int iRet = -1;
-    if (len < sizeof(IGMPHdrBase))
-    {
+    if (len < sizeof(IGMPHdrBase)) {
         ENGINE_SET_INVALID_EVENT(p, IGMP_MSG_LEN_TOO_SMALL);
         return iRet;
     }
@@ -76,8 +68,7 @@ static int IgmpParseV1V2(Packet *p,const uint8_t *buffer, size_t len)
 
     printf("  IGMP version: %d\n", header->ucType == IGMP_V1_MEMBERSHIP_REPORT ? 1 : 2);
     printf("  Message type: ");
-    switch (header->ucType)
-    {
+    switch (header->ucType) {
         case IGMP_MEMBERSHIP_QUERY:
             printf("IGMP_MEMBERSHIP_QUERY\n");
             break;
@@ -96,8 +87,7 @@ static int IgmpParseV1V2(Packet *p,const uint8_t *buffer, size_t len)
             return -1;
     }
 
-    if (header->ucType == IGMP_MEMBERSHIP_QUERY || header->ucType == IGMP_V2_MEMBERSHIP_REPORT)
-    {
+    if (header->ucType == IGMP_MEMBERSHIP_QUERY || header->ucType == IGMP_V2_MEMBERSHIP_REPORT) {
         printf("  max response time: %ds\n", header->ucMaxRespTime / 10);
     }
 
@@ -121,44 +111,35 @@ static int DecodeIGMPPacket(ThreadVars *tv, Packet *p, const uint8_t *pkt, uint1
 
     DEBUG_VALIDATE_BUG_ON(pkt == NULL);
 
-    if (unlikely(len < sizeof(IGMPHdrBase)))
-    {
+    if (unlikely(len < sizeof(IGMPHdrBase))) {
         ENGINE_SET_INVALID_EVENT(p, IGMP_HEADER_TOO_SMALL);
         return iRet;
     }
-    if (!PacketIncreaseCheckLayers(p))
-    {
+    if (!PacketIncreaseCheckLayers(p)) {
         return iRet;
     }
     p->igmp = (IGMPHdrBase *)pkt;
 
-    switch (p->igmp->ucType)
-    {
+    switch (p->igmp->ucType) {
         case IGMP_MEMBERSHIP_QUERY:
-            if (len > 8 && (pkt[1] & 0x0F) != 0)
-            {
-                iRet = IgmpParseV3Query(p,pkt, len);
-            }
-            else
-            {
-                iRet = IgmpParseV1V2(p,pkt, len);
+            if (len > 8 && (pkt[1] & 0x0F) != 0) {
+                iRet = IgmpParseV3Query(p, pkt, len);
+            } else {
+                iRet = IgmpParseV1V2(p, pkt, len);
             }
             break;
         case IGMP_V1_MEMBERSHIP_REPORT:
-            iRet = IgmpParseV1V2(p,pkt, len);
+            iRet = IgmpParseV1V2(p, pkt, len);
             break;
         case IGMP_V2_MEMBERSHIP_REPORT:
-            if (len > 8 && pkt[2] == 0 && pkt[3] == 0)
-            {
-                iRet = ParseIGMPV3Report(p,pkt, len);
-            }
-            else
-            {
-                iRet = IgmpParseV1V2(p,pkt, len);
+            if (len > 8 && pkt[2] == 0 && pkt[3] == 0) {
+                iRet = ParseIGMPV3Report(p, pkt, len);
+            } else {
+                iRet = IgmpParseV1V2(p, pkt, len);
             }
             break;
         case IGMP_LEAVE_GROUP:
-            iRet = IgmpParseV1V2(p,pkt, len);
+            iRet = IgmpParseV1V2(p, pkt, len);
             break;
         default:
             ENGINE_SET_INVALID_EVENT(p, IGMP_UNSUPPORTED_MST_TYPE);
@@ -168,12 +149,10 @@ static int DecodeIGMPPacket(ThreadVars *tv, Packet *p, const uint8_t *pkt, uint1
     return TM_ECODE_OK;
 }
 
-
 int DecodeIGMP(ThreadVars *tv, DecodeThreadVars *dtv, Packet *p, const uint8_t *pkt, uint32_t len)
 {
     StatsIncr(tv, dtv->counter_igmp);
-    if (unlikely(DecodeIGMPPacket(tv, p,pkt,len) < 0))
-    {
+    if (unlikely(DecodeIGMPPacket(tv, p, pkt, len) < 0)) {
         CLEAR_IGMP_PACKET(p);
         return TM_ECODE_FAILED;
     }
@@ -182,13 +161,12 @@ int DecodeIGMP(ThreadVars *tv, DecodeThreadVars *dtv, Packet *p, const uint8_t *
     return TM_ECODE_OK;
 }
 
-
 #ifdef UNITTESTS
 /**
  * \test DecodeIGMP
  */
 
-static int DecodeIGMPtest (void)
+static int DecodeIGMPtest(void)
 {
     PASS;
 }
--- src/decode-igmp.h
+++ src/decode-igmp.h
@@ -5,71 +5,64 @@
 #define IPPROTO_IGMP 2
 #endif
 
-
-
 #define IGMP_VERSION_1 1
 #define IGMP_VERSION_2 2
 #define IGMP_VERSION_3 3
 
 // IGMP v1 and v2 message type
-#define IGMP_MEMBERSHIP_QUERY        0x11
-#define IGMP_V1_MEMBERSHIP_REPORT    0x12
-#define IGMP_V2_MEMBERSHIP_REPORT    0x22
-#define IGMP_LEAVE_GROUP             0x23
+#define IGMP_MEMBERSHIP_QUERY     0x11
+#define IGMP_V1_MEMBERSHIP_REPORT 0x12
+#define IGMP_V2_MEMBERSHIP_REPORT 0x22
+#define IGMP_LEAVE_GROUP          0x23
 
-#define IGMP_V3_MEMBERSHIP_REPORT    0x22
+#define IGMP_V3_MEMBERSHIP_REPORT 0x22
 
-#define MODE_IS_INCLUDE       1
-#define MODE_IS_EXCLUDE       2
-#define CHANGE_TO_INCLUDE     3
-#define CHANGE_TO_EXCLUDE     4
-#define ALLOW_NEW_SOURCES     5
-#define BLOCK_OLD_SOURCES     6
+#define MODE_IS_INCLUDE   1
+#define MODE_IS_EXCLUDE   2
+#define CHANGE_TO_INCLUDE 3
+#define CHANGE_TO_EXCLUDE 4
+#define ALLOW_NEW_SOURCES 5
+#define BLOCK_OLD_SOURCES 6
 
-typedef struct tagStIGMPV3QueryHdr
-{
-    unsigned char ucType; //0x22
+typedef struct tagStIGMPV3QueryHdr {
+    unsigned char ucType; // 0x22
     unsigned char ucMaxRespTimeFlags;
     unsigned short usChecksum;
-    unsigned char  ucFlagQrvQqic; // 1 bit flag;3 bits qrv; 4 bits qqic
+    unsigned char ucFlagQrvQqic; // 1 bit flag;3 bits qrv; 4 bits qqic
     unsigned short usNumSources;
-    unsigned int   *uipSourceAddrs;
-}IGMPV3QueryHdr;
+    unsigned int *uipSourceAddrs;
+} IGMPV3QueryHdr;
 
-
-typedef struct tagStIGMPV3GroupRecord
-{
+typedef struct tagStIGMPV3GroupRecord {
     unsigned char ucRecordType;
     unsigned char ucAuxDataLen;
     unsigned short usNumSources;
-    unsigned int   uiMulticastAddr;
-    unsigned int   *puiSourceAddrs;
-}IGMPV3GroupRecord;
+    unsigned int uiMulticastAddr;
+    unsigned int *puiSourceAddrs;
+} IGMPV3GroupRecord;
 
-typedef struct tagStIGMPV3ReportHdr
-{
-    unsigned char ucType; //0x022
+typedef struct tagStIGMPV3ReportHdr {
+    unsigned char ucType; // 0x022
     unsigned char ucResv1;
     unsigned short usChecksum;
     unsigned short ucResv2;
     unsigned short usNumGroupRecords;
-    IGMPV3GroupRecord  *pstRecords;
-}IGMPV3ReportHdr;
+    IGMPV3GroupRecord *pstRecords;
+} IGMPV3ReportHdr;
 
-typedef struct tagStIGMPHdrBase
-{
-    unsigned char  ucType;
-    unsigned char  ucMaxRespTime;
+typedef struct tagStIGMPHdrBase {
+    unsigned char ucType;
+    unsigned char ucMaxRespTime;
     unsigned short usChecksum;
-    unsigned int   uiGroupAddr;
-}IGMPHdrBase;
+    unsigned int uiGroupAddr;
+} IGMPHdrBase;
 
-
-#define CLEAR_IGMP_PACKET(p) { \
-    (p)->igmp = NULL; \
-} while (0)
+#define CLEAR_IGMP_PACKET(p)                                                                       \
+    {                                                                                              \
+        (p)->igmp = NULL;                                                                          \
+    }                                                                                              \
+    while (0)
 
 void DecodeIGMPRegisterTests(void);
-
 
 #endif
